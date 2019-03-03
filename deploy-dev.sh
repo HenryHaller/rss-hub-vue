3a13f1b3684763810adcbf48e26da1ea06089717
Henry Haller <henrybhaller@gmail.com>
2019-03-04 00:30:33 +0900

    add git comands to deploy scritps

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,3 +1,4 @@
 cd /home/henry/code/HenryHaller/rss-hub-vue
+git co dev
 yarn build --mode development
 firebase deploy --project rss-view-dev
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 cd /home/henry/code/HenryHaller/rss-hub-vue
+git co master
 yarn build
 firebase deploy --project rsshub-vue
