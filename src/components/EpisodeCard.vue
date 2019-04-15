14238b47c39d7054a04c824ae3bc020f7e52afc0
Henry Haller <henrybhaller@gmail.com>
2019-04-15 11:03:01 +0900

    episode card positioning is ok

--- src/components/EpisodeCard.vue
+++ src/components/EpisodeCard.vue
@@ -15,10 +15,10 @@
           <span class="description-text" v-html="episode.description"></span>
         </div>
         <div class="download-box">
-          <a :href="episode.url">Download</a>
-          <router-link :to="{ name: 'Show', params: { id: episode.show_id } }">
-            All Episodes
-          </router-link>
+          <a :href="episode.url">&#x2B73; Download &#x2B73;</a>
+          <router-link :to="{ name: 'Show', params: { id: episode.show_id } }"
+            >All Episodes</router-link
+          >
         </div>
       </div>
     </div>
@@ -44,6 +44,77 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.episode-box {
+  .description-box {
+    h3 {
+      text-align: center;
+    }
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    width: 100%;
+    align-content: center;
+    .download-box {
+      display: flex;
+      justify-content: space-around;
+      // width: 100%;
+    }
+    .description-text-box {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 0.75rem;
+      .description-text {
+        text-indent: 2rem;
+      }
+    }
+    color: silver;
+    padding: 0.5rem 1rem;
+    h3 {
+      margin-top: 0.5rem;
+    }
+  }
+
+  .icon-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0.5rem 0 0.5rem 0;
+    flex-direction: column;
+    justify-content: space-around;
+    .episode-icon {
+      display: flex;
+      flex-direction: column-reverse;
+      span {
+        white-space: pre;
+        color: silver;
+        background-color: #111;
+      }
+      border-radius: 50%;
+      margin: 1em;
+      box-shadow: 0px -7px 3px 6px;
+      background-size: auto 100%;
+      background-repeat: no-repeat;
+      background-position: left top;
+      position: relative;
+      p {
+        text-align: center;
+      }
+      width: 128px;
+      height: 128px;
+      //flex-direction: column;
+    }
+  }
+  border-radius: 10px;
+  padding: 0.5rem;
+  background-color: #333;
+  display: flex;
+  margin: 1rem 0;
+  @media screen and (max-width: 578px) {
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
 .ep-enter-active {
   transition: opacity 4s;
 }
@@ -53,85 +124,5 @@ export default {
 
 .ep-leave-to {
   opacity: 1;
-}
-
-a {
-  color: silver;
-  text-decoration: none;
-}
-.icon-box {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0.5rem 0 0.5rem 0;
-  flex-direction: column;
-  justify-content: space-around;
-}
-.episode-box {
-  border-radius: 10px;
-  padding: 0.5rem;
-  background-color: #333;
-  display: flex;
-  margin: 1rem 0;
-}
-
-.title {
-  text-align: center;
-}
-
-@media screen and (max-width: 578px) {
-  .episode-box {
-    flex-direction: column;
-    align-items: center;
-  }
-}
-
-.description-box {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  width: 100%;
-  align-content: center;
-  .download-box {
-    display: flex;
-    justify-content: space-around;
-    // width: 100%;
-  }
-  .description-text-box {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 0.75rem;
-    .description-text {
-      text-indent: 2rem;
-    }
-  }
-  color: silver;
-  padding: 0.5rem 1rem;
-  h3 {
-    margin-top: 0.5rem;
-  }
-}
-.episode-icon {
-  display: flex;
-  flex-direction: column-reverse;
-
-  span {
-    white-space: pre;
-    color: silver;
-    background-color: #111;
-  }
-  border-radius: 50%;
-  margin: 1em;
-  box-shadow: 0px -7px 3px 6px;
-  background-size: auto 100%;
-  background-repeat: no-repeat;
-  background-position: left top;
-  position: relative;
-  p {
-    text-align: center;
-  }
-  width: 128px;
-  height: 128px;
-  //flex-direction: column;
 }
 </style>
