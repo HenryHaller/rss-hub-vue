51284294bb03c8278a53ce34c921fcb8c7a96539
Henry Haller <henrybhaller@gmail.com>
2019-04-23 18:20:47 +0900

    fix for loading more episodes by scrolling in management section

--- src/components/EpisodeList.vue
+++ src/components/EpisodeList.vue
@@ -1,6 +1,10 @@
 <template>
   <transition name="fade" mode="out-in">
-    <div v-if="!initialUpdating && episodes.length > 0" class="episode-list" key="episodeList">
+    <div
+      v-if="!initialUpdating && episodes.length > 0"
+      class="episode-list"
+      key="episodeList"
+    >
       <EpisodeCard
         v-for="episode in episodes"
         :key="episode.id"
@@ -10,11 +14,9 @@
       />
       <div v-if="listExhausted" class="no-more-shows">&#x1F6AB;</div>
     </div>
-    <div
-      class="no-shows"
-      v-else-if="!initialUpdating"
-      key="empty"
-    >You have no episodes. Try subscribing to some shows?</div>
+    <div class="no-shows" v-else-if="!initialUpdating" key="empty">
+      You have no episodes. Try subscribing to some shows?
+    </div>
     <div class="no-shows" v-else key="rotating">
       <div class="rotate-forever big-size">&#x27F3;</div>
     </div>
@@ -42,7 +44,10 @@ export default {
       setUpdateIntervalKey: "RSSHub/setUpdateIntervalKey"
     }),
     loadNextPage() {
-      if (this.listExhausted === false) {
+      const feedActive = document
+        .querySelector("#feedLink")
+        .classList.contains("active");
+      if (this.listExhausted === false && feedActive === true) {
         if (
           window.innerHeight + Math.ceil(window.pageYOffset + 1) >=
           document.body.offsetHeight
