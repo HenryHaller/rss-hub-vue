bfe38ccfc0aedfe85ee5e146d8c556bb54be3694
Henry Haller <henrybhaller@gmail.com>
2019-05-01 15:18:25 +0900

    add error page

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,59 @@
 <template>
-  <div class="main d-none">
+  <div class="main">
     <Header />
     <flash-message class="flash-messages"></flash-message>
-    <router-view />
+    <div v-if="loading" style="min-height: calc(100vh - 120px);">!</div>
+    <router-view v-else />
     <Footer text="Copyright 2019 Henry Haller" />
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
-import UserService from "@/services/UserService";
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import UserService from '@/services/UserService'
 
 export default {
   components: {
     Header,
     Footer
   },
-  beforeCreate() {
-    document.title = "RSSHub";
+  data() {
+    return {
+      loading: true
+    }
+  },
 
-    if (process.env.NODE_ENV === "development")
-      document.title += "(development)";
+  beforeCreate() {
+    document.title = 'RSSHub'
+
+    if (process.env.NODE_ENV === 'development') document.title += '(development)'
 
     UserService.checkLogin()
       .then(response => {
-        this.loginCheckCompleted = true;
-        this.$el.classList.remove("d-none");
+        this.loginCheckCompleted = true
 
         if (response.status === 204) {
-          if (this.$route.name !== "Show") {
-            this.$router.push({ name: "Episodes" });
+          if (this.$route.name !== 'Show') {
+            this.$router.push({ name: 'Episodes' })
           }
         } else {
-          if (this.$route.name !== "Recover")
-            this.$router.push({ name: "Home" });
+          if (this.$route.name !== 'Recover') this.$router.push({ name: 'Home' })
         }
       })
       .catch(err => {
-        this.loginCheckCompleted = true;
-      });
+        console.log(err)
+        this.$router.push({ name: 'ApplicationError' })
+      })
+      .finally(() => {
+        this.loading = false
+      })
   }
-};
+}
 </script>
 
 <style lang="scss">
-@import "src/assets/stylesheets/application.scss";
+@import 'src/assets/stylesheets/application.scss';
 // $background-lighter: #0b3337;
 // $background-darker: #13262f;
 
--- src/router.js
+++ src/router.js
@@ -1,42 +1,48 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
-import Episodes from "./views/Episodes.vue";
-import PendingActivation from "./views/PendingActivation.vue";
-import Activate from "./views/Activate.vue";
-import PasswordRecoveryAttempt from "./views/PasswordRecoveryAttempt.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Episodes from './views/Episodes.vue'
+import PendingActivation from './views/PendingActivation.vue'
+import Activate from './views/Activate.vue'
+import PasswordRecoveryAttempt from './views/PasswordRecoveryAttempt.vue'
+import ApplicationError from './views/ApplicationError.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "Home",
+      path: '/',
+      name: 'Home',
       component: Home
     },
     {
-      path: "/episodes",
-      name: "Episodes",
+      path: '/episodes',
+      name: 'Episodes',
       component: Episodes
     },
-    { path: "/show/:id", name: "Show", component: Episodes },
+    { path: '/show/:id', name: 'Show', component: Episodes },
     {
-      path: "/pending_activation",
-      name: "PendingActivation",
+      path: '/pending_activation',
+      name: 'PendingActivation',
       component: PendingActivation
     },
     {
-      path: "/activate",
-      name: "Activate",
+      path: '/activate',
+      name: 'Activate',
       component: Activate
     },
     {
-      path: "/recover",
-      name: "Recover",
+      path: '/recover',
+      name: 'Recover',
       component: PasswordRecoveryAttempt
+    },
+    {
+      path: '/error',
+      name: 'ApplicationError',
+      component: ApplicationError
     }
   ]
-});
+})
